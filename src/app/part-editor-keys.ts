7d4eb3d8d2e18ef2f065b3250f82e10b18efee3d
Myrmex <dfusi@hotmail.com>
2022-01-21 16:07:03 +0100

    Initial commit

---
+++ src/app/part-editor-keys.ts
@@ -0,0 +1,15 @@
+import { PartEditorKeys } from '@myrmidon/cadmus-core';
+
+const CODICOLOGY = 'codicology';
+
+/**
+ * The parts and fragments editor keys for this UI.
+ * Each property is a part type ID, mapped to a value of type PartGroupKey,
+ * having a part property with the part's editor key, and a fragments property
+ * with the mappings between fragment type IDs and their editor keys.
+ */
+export const PART_EDITOR_KEYS: PartEditorKeys = {
+  // [BIBLIOGRAPHY_PART_TYPEID]: {
+  //   part: GENERAL,
+  // },
+};
