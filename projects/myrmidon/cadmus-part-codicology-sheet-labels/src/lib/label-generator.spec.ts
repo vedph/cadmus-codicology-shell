3bcc69da11af996fcb359d59ca197c44dc835738
Myrmex <dfusi@hotmail.com>
2022-02-19 15:11:33 +0100

    Implementing quire action

--- projects/myrmidon/cadmus-part-codicology-sheet-labels/src/lib/label-generator.spec.ts
+++ projects/myrmidon/cadmus-part-codicology-sheet-labels/src/lib/label-generator.spec.ts
@@ -1,4 +1,4 @@
-import { LabelActionValueType, LabelGenerator } from './label-generator';
+import { LabelActionType, LabelGenerator } from './label-generator';
 
 describe('LabelGenerator', () => {
   // parser
@@ -21,163 +21,174 @@ describe('LabelGenerator', () => {
   it('should parse "1x2="', () => {
     const action = LabelGenerator.parseAction('1x2=');
     expect(action).toBeTruthy();
-    expect(action!.start).toBe(1);
+    expect(action!.n).toBe(1);
     expect(action!.v).toBeFalse();
     expect(action!.page).toBeFalsy();
     expect(action!.count).toBe(2);
     expect(action!.value).toBeFalsy();
-    expect(action!.valueType).toBe(LabelActionValueType.Custom);
+    expect(action!.type).toBe(LabelActionType.Custom);
   });
   it('should parse "1x2=custom"', () => {
     const action = LabelGenerator.parseAction('1x2=custom');
     expect(action).toBeTruthy();
-    expect(action!.start).toBe(1);
+    expect(action!.n).toBe(1);
     expect(action!.v).toBeFalse();
     expect(action!.page).toBeFalsy();
     expect(action!.count).toBe(2);
     expect(action!.value).toBe('custom');
-    expect(action!.valueType).toBe(LabelActionValueType.Custom);
+    expect(action!.type).toBe(LabelActionType.Custom);
   });
   it('should parse "1rx2=custom"', () => {
     const action = LabelGenerator.parseAction('1rx2=custom');
     expect(action).toBeTruthy();
-    expect(action!.start).toBe(1);
+    expect(action!.n).toBe(1);
     expect(action!.v).toBeFalse();
     expect(action!.page).toBeFalsy();
     expect(action!.count).toBe(2);
     expect(action!.value).toBe('custom');
-    expect(action!.valueType).toBe(LabelActionValueType.Custom);
+    expect(action!.type).toBe(LabelActionType.Custom);
   });
   it('should parse "1vx2=custom"', () => {
     const action = LabelGenerator.parseAction('1vx2=custom');
     expect(action).toBeTruthy();
-    expect(action!.start).toBe(1);
+    expect(action!.n).toBe(1);
     expect(action!.v).toBeTrue();
     expect(action!.page).toBeFalsy();
     expect(action!.count).toBe(2);
     expect(action!.value).toBe('custom');
-    expect(action!.valueType).toBe(LabelActionValueType.Custom);
+    expect(action!.type).toBe(LabelActionType.Custom);
   });
   it('should parse "1r*2=custom"', () => {
     const action = LabelGenerator.parseAction('1r*2=custom');
     expect(action).toBeTruthy();
-    expect(action!.start).toBe(1);
+    expect(action!.n).toBe(1);
     expect(action!.v).toBeFalse();
     expect(action!.page).toBeFalsy();
     expect(action!.count).toBe(2);
     expect(action!.value).toBe('custom');
-    expect(action!.valueType).toBe(LabelActionValueType.Custom);
+    expect(action!.type).toBe(LabelActionType.Custom);
   });
   it('should parse "1r%2=custom"', () => {
     const action = LabelGenerator.parseAction('1r%2=custom');
     expect(action).toBeTruthy();
-    expect(action!.start).toBe(1);
+    expect(action!.n).toBe(1);
     expect(action!.v).toBeFalse();
     expect(action!.page).toBeTrue();
     expect(action!.count).toBe(2);
     expect(action!.value).toBe('custom');
-    expect(action!.valueType).toBe(LabelActionValueType.Custom);
+    expect(action!.type).toBe(LabelActionType.Custom);
   });
   it('should parse "1r x 2 = custom"', () => {
     const action = LabelGenerator.parseAction('1r x 2 = custom');
     expect(action).toBeTruthy();
-    expect(action!.start).toBe(1);
+    expect(action!.n).toBe(1);
     expect(action!.v).toBeFalse();
     expect(action!.page).toBeFalsy();
     expect(action!.count).toBe(2);
     expect(action!.value).toBe('custom');
-    expect(action!.valueType).toBe(LabelActionValueType.Custom);
+    expect(action!.type).toBe(LabelActionType.Custom);
   });
   it('should parse "1x2=12"', () => {
     const action = LabelGenerator.parseAction('1x2=12');
     expect(action).toBeTruthy();
-    expect(action!.start).toBe(1);
+    expect(action!.n).toBe(1);
     expect(action!.v).toBeFalse();
     expect(action!.page).toBeFalsy();
     expect(action!.count).toBe(2);
     expect(action!.value).toBe('12');
-    expect(action!.valueType).toBe(LabelActionValueType.Arabic);
+    expect(action!.type).toBe(LabelActionType.Arabic);
   });
   it('should parse "1x2=II"', () => {
     const action = LabelGenerator.parseAction('1x2=II');
     expect(action).toBeTruthy();
-    expect(action!.start).toBe(1);
+    expect(action!.n).toBe(1);
     expect(action!.v).toBeFalse();
     expect(action!.page).toBeFalsy();
     expect(action!.count).toBe(2);
     expect(action!.value).toBe('II');
-    expect(action!.valueType).toBe(LabelActionValueType.UpperRoman);
+    expect(action!.type).toBe(LabelActionType.UpperRoman);
   });
   it('should parse "1x2=X"', () => {
     const action = LabelGenerator.parseAction('1x2=X');
     expect(action).toBeTruthy();
-    expect(action!.start).toBe(1);
+    expect(action!.n).toBe(1);
     expect(action!.v).toBeFalse();
     expect(action!.page).toBeFalsy();
     expect(action!.count).toBe(2);
     expect(action!.value).toBe('X');
-    expect(action!.valueType).toBe(LabelActionValueType.UpperRoman);
+    expect(action!.type).toBe(LabelActionType.UpperRoman);
   });
   it('should parse "1x2="X" as custom"', () => {
     const action = LabelGenerator.parseAction('1x2="X"');
     expect(action).toBeTruthy();
-    expect(action!.start).toBe(1);
+    expect(action!.n).toBe(1);
     expect(action!.v).toBeFalse();
     expect(action!.page).toBeFalsy();
     expect(action!.count).toBe(2);
     expect(action!.value).toBe('X');
-    expect(action!.valueType).toBe(LabelActionValueType.Custom);
+    expect(action!.type).toBe(LabelActionType.Custom);
   });
   it('should parse "1x2=ii"', () => {
     const action = LabelGenerator.parseAction('1x2=ii');
     expect(action).toBeTruthy();
-    expect(action!.start).toBe(1);
+    expect(action!.n).toBe(1);
     expect(action!.v).toBeFalse();
     expect(action!.page).toBeFalsy();
     expect(action!.count).toBe(2);
     expect(action!.value).toBe('ii');
-    expect(action!.valueType).toBe(LabelActionValueType.LowerRoman);
+    expect(action!.type).toBe(LabelActionType.LowerRoman);
   });
   it('should parse "1x2=b"', () => {
     const action = LabelGenerator.parseAction('1x2=b');
     expect(action).toBeTruthy();
-    expect(action!.start).toBe(1);
+    expect(action!.n).toBe(1);
     expect(action!.v).toBeFalse();
     expect(action!.page).toBeFalsy();
     expect(action!.count).toBe(2);
     expect(action!.value).toBe('b');
-    expect(action!.valueType).toBe(LabelActionValueType.LatLowerLetter);
+    expect(action!.type).toBe(LabelActionType.LatLowerLetter);
   });
   it('should parse "1x2=B"', () => {
     const action = LabelGenerator.parseAction('1x2=B');
     expect(action).toBeTruthy();
-    expect(action!.start).toBe(1);
+    expect(action!.n).toBe(1);
     expect(action!.v).toBeFalse();
     expect(action!.page).toBeFalsy();
     expect(action!.count).toBe(2);
     expect(action!.value).toBe('B');
-    expect(action!.valueType).toBe(LabelActionValueType.LatUpperLetter);
+    expect(action!.type).toBe(LabelActionType.LatUpperLetter);
   });
   it('should parse "1x2=Β"', () => {
     const action = LabelGenerator.parseAction('1x2=Β');
     expect(action).toBeTruthy();
-    expect(action!.start).toBe(1);
+    expect(action!.n).toBe(1);
     expect(action!.v).toBeFalse();
     expect(action!.page).toBeFalsy();
     expect(action!.count).toBe(2);
     expect(action!.value).toBe('Β');
-    expect(action!.valueType).toBe(LabelActionValueType.GrcUpperLetter);
+    expect(action!.type).toBe(LabelActionType.GrcUpperLetter);
   });
   it('should parse "1x2=β"', () => {
     const action = LabelGenerator.parseAction('1x2=β');
     expect(action).toBeTruthy();
-    expect(action!.start).toBe(1);
+    expect(action!.n).toBe(1);
     expect(action!.v).toBeFalse();
     expect(action!.page).toBeFalsy();
     expect(action!.count).toBe(2);
     expect(action!.value).toBe('β');
-    expect(action!.valueType).toBe(LabelActionValueType.GrcLowerLetter);
+    expect(action!.type).toBe(LabelActionType.GrcLowerLetter);
   });
+  it('should parse "1x2=q1/4"', () => {
+    const action = LabelGenerator.parseAction('1x2=q1/4');
+    expect(action).toBeTruthy();
+    expect(action!.n).toBe(1);
+    expect(action!.v).toBeFalse();
+    expect(action!.page).toBeFalsy();
+    expect(action!.count).toBe(2);
+    expect(action!.value).toBe('q1/4');
+    expect(action!.type).toBe(LabelActionType.Quire);
+  });
+
   // generator
   it('should generate 3 from 1rx3=1', () => {
     const cells = LabelGenerator.generateFrom('1x3=1');
@@ -187,6 +198,23 @@ describe('LabelGenerator', () => {
       expect(cells[i].rowId).toBe(`${i + 1}r`);
       expect(cells[i].value).toBe(`${i + 1}`);
     }
+  });
+  it('should generate 3 from 1r%3=1', () => {
+    const cells = LabelGenerator.generateFrom('1%3=1');
+    expect(cells).toBeTruthy();
+    expect(cells.length).toBe(3);
+
+    let cell = cells[0];
+    expect(cell.rowId).toBe('1r');
+    expect(cell.value).toBe('1');
+
+    cell = cells[1];
+    expect(cell.rowId).toBe('1v');
+    expect(cell.value).toBe('2');
+
+    cell = cells[2];
+    expect(cell.rowId).toBe('2r');
+    expect(cell.value).toBe('3');
   });
   it('should generate 3 from 1vx3=X', () => {
     const cells = LabelGenerator.generateFrom('1vx3=X');
@@ -204,5 +232,68 @@ describe('LabelGenerator', () => {
     cell = cells[2];
     expect(cell.rowId).toBe('3v');
     expect(cell.value).toBe('XII');
+  });
+  it('should generate 16 from 1x2=q1/4', () => {
+    const cells = LabelGenerator.generateFrom('1x2=q1/4');
+    expect(cells).toBeTruthy();
+    expect(cells.length).toBe(16);
+
+    // 1st quire
+    let cell = cells[0];
+    expect(cell.rowId).toBe('1r');
+    expect(cell.value).toBe('1.1/4');
+    cell = cells[1];
+    expect(cell.rowId).toBe('1v');
+    expect(cell.value).toBe('1.1/4');
+
+    cell = cells[2];
+    expect(cell.rowId).toBe('2r');
+    expect(cell.value).toBe('1.2/4');
+    cell = cells[3];
+    expect(cell.rowId).toBe('2v');
+    expect(cell.value).toBe('1.2/4');
+
+    cell = cells[4];
+    expect(cell.rowId).toBe('3r');
+    expect(cell.value).toBe('1.3/4');
+    cell = cells[5];
+    expect(cell.rowId).toBe('3v');
+    expect(cell.value).toBe('1.3/4');
+
+    cell = cells[6];
+    expect(cell.rowId).toBe('4r');
+    expect(cell.value).toBe('1.4/4');
+    cell = cells[7];
+    expect(cell.rowId).toBe('4v');
+    expect(cell.value).toBe('1.4/4');
+
+    // 2nd quire
+    cell = cells[8];
+    expect(cell.rowId).toBe('5r');
+    expect(cell.value).toBe('2.1/4');
+    cell = cells[9];
+    expect(cell.rowId).toBe('5v');
+    expect(cell.value).toBe('2.1/4');
+
+    cell = cells[10];
+    expect(cell.rowId).toBe('6r');
+    expect(cell.value).toBe('2.2/4');
+    cell = cells[11];
+    expect(cell.rowId).toBe('6v');
+    expect(cell.value).toBe('2.2/4');
+
+    cell = cells[12];
+    expect(cell.rowId).toBe('7r');
+    expect(cell.value).toBe('2.3/4');
+    cell = cells[13];
+    expect(cell.rowId).toBe('7v');
+    expect(cell.value).toBe('2.3/4');
+
+    cell = cells[14];
+    expect(cell.rowId).toBe('8r');
+    expect(cell.value).toBe('2.4/4');
+    cell = cells[15];
+    expect(cell.rowId).toBe('8v');
+    expect(cell.value).toBe('2.4/4');
   });
 });
--- projects/myrmidon/cadmus-part-codicology-sheet-labels/src/lib/label-generator.ts
+++ projects/myrmidon/cadmus-part-codicology-sheet-labels/src/lib/label-generator.ts
@@ -7,15 +7,17 @@ export interface LabelCell {
 }
 
 export interface LabelAction {
-  start: number;
+  type: LabelActionType;
+  n: number;
   v: boolean;
+  qn?: number;
+  qs?: number;
   page?: boolean;
   count: number;
   value?: string;
-  valueType: LabelActionValueType;
 }
 
-export enum LabelActionValueType {
+export enum LabelActionType {
   Custom = 0,
   Arabic,
   UpperRoman,
@@ -24,17 +26,19 @@ export enum LabelActionValueType {
   LatLowerLetter,
   GrcUpperLetter,
   GrcLowerLetter,
+  Quire,
 }
 
 /**
  * Labels generator.
  */
 export class LabelGenerator {
-  // 1=start nr
-  // 2=r/v/nothing
+  // 1=n
+  // 2=r/v
   // 3=* (sheet) or % (page)
   // 4=count
-  // 5=value (if in "" this forces custom type)
+  // 5=value (if in "" this forces custom type; if qN/N is a quire;
+  //          for quires we assume always r, never v).
   public static PATTERN: RegExp =
     /([0-9]+)([rv])?\s*([x*%])\s*([0-9]+)\s*=\s*([^\s]*)/;
 
@@ -55,51 +59,65 @@ export class LabelGenerator {
     }
 
     const action: LabelAction = {
-      start: +m[1],
+      n: +m[1],
       v: m[2] === 'v',
       page: m[3] === '%',
       count: +m[4],
       value: m[5] || '',
-      valueType: LabelActionValueType.Custom,
+      type: LabelActionType.Custom,
     };
 
-    // corner case: value in ""
-    if (
-      action.value!.length > 2 &&
-      action.value!.charAt(0) === '"' &&
-      action.value!.charAt(action.value!.length - 1) === '"'
-    ) {
-      action.value = action.value!.substring(1, action.value!.length - 1);
-      return action;
+    // handle corner cases for value first
+    if (action.value) {
+      // corner case: value in ""
+      if (
+        action.value.length > 2 &&
+        action.value.charAt(0) === '"' &&
+        action.value.charAt(action.value.length - 1) === '"'
+      ) {
+        action.value = action.value.substring(1, action.value.length - 1);
+        return action;
+      }
+
+      // corner case: value like qN/N (quire)
+      const qm = /^q([0-9]+)\/([0-9]+)$/.exec(action.value);
+      if (qm) {
+        action.type = LabelActionType.Quire;
+        action.v = false;
+        action.page = false;
+        action.qn = +m[1];
+        action.qs = +m[2];
+        return action;
+      }
     }
 
     // determine value type:
     // - Arabic number
     if (/^[0-9]+$/.test(action.value!)) {
-      action.valueType = LabelActionValueType.Arabic;
+      action.type = LabelActionType.Arabic;
       return action;
     }
     // - Roman number
     if (/^[IVXLCM]+$/.test(action.value!)) {
-      action.valueType = LabelActionValueType.UpperRoman;
+      action.type = LabelActionType.UpperRoman;
     } else if (/^[ivxlcm]+$/.test(action.value!)) {
-      action.valueType = LabelActionValueType.LowerRoman;
+      action.type = LabelActionType.LowerRoman;
     }
     // if mixed case, assume uppercase
     else if (/^[ivxlcmIVXLCM]+$/.test(action.value!)) {
-      action.valueType = LabelActionValueType.UpperRoman;
+      action.type = LabelActionType.UpperRoman;
     }
     // - single Latin letter
     else if (/^[a-z]$/.test(action.value!)) {
-      action.valueType = LabelActionValueType.LatLowerLetter;
+      action.type = LabelActionType.LatLowerLetter;
     } else if (/^[A-Z]$/.test(action.value!)) {
-      action.valueType = LabelActionValueType.LatUpperLetter;
+      action.type = LabelActionType.LatUpperLetter;
     }
     // - single Greek letter (Unicode alphabet, excluding waw koppa sampi)
     else if (/^[α-ω]$/.test(action.value!)) {
-      action.valueType = LabelActionValueType.GrcLowerLetter;
+      action.type = LabelActionType.GrcLowerLetter;
     } else if (/^[Α-Ω]$/.test(action.value!)) {
-      action.valueType = LabelActionValueType.GrcUpperLetter;
+      action.type = LabelActionType.GrcUpperLetter;
     }
     return action;
   }
@@ -127,6 +145,33 @@ export class LabelGenerator {
     }
   }
 
+  private static generateQuireRows(action: LabelAction): LabelCell[] {
+    const cells: LabelCell[] = [];
+    if (!action.qn) {
+      return cells;
+    }
+    let n = action.n; // row number
+    let qn = action.qn; // quire number
+    let sn = 1; // sheet number
+
+    // generate qn.sn/qs for each pair of r/v rows
+    for (let i = 0; i < action.count * action.qn; i++) {
+      const value = `${qn + i}.${sn}/${action.qs}`;
+      cells.push({
+        rowId: n.toString() + 'r',
+        value: value,
+      });
+      cells.push({
+        rowId: n.toString() + 'v',
+        value: value,
+      });
+      if (++sn > action.qs!) {
+        sn = 1;
+      }
+    }
+    return cells;
+  }
+
   /**
    * Generate a set of label cells from the specified action.
    * @param action The action.
@@ -136,8 +181,13 @@ export class LabelGenerator {
     if (!action) {
       return [];
     }
+    // quire is a corner case
+    if (action.type === LabelActionType.Quire) {
+      return this.generateQuireRows(action);
+    }
+
     const cells: LabelCell[] = [];
-    let n = action.start;
+    let n = action.n;
     let v = action.v;
     let value = action.value;
     for (let i = 0; i < action.count; i++) {
@@ -157,26 +207,26 @@ export class LabelGenerator {
         n++;
       }
       // calculate new value
-      switch (action.valueType) {
-        case LabelActionValueType.Arabic:
+      switch (action.type) {
+        case LabelActionType.Arabic:
           value = (+value! + 1).toString();
           break;
-        case LabelActionValueType.UpperRoman:
+        case LabelActionType.UpperRoman:
           value = this.getNextRoman(value!, false);
           break;
-        case LabelActionValueType.LowerRoman:
+        case LabelActionType.LowerRoman:
           value = this.getNextRoman(value!, true);
           break;
-        case LabelActionValueType.LatLowerLetter:
+        case LabelActionType.LatLowerLetter:
           value = this.getNextLatLetter(value!, true);
           break;
-        case LabelActionValueType.LatUpperLetter:
+        case LabelActionType.LatUpperLetter:
           value = this.getNextLatLetter(value!, false);
           break;
-        case LabelActionValueType.GrcLowerLetter:
+        case LabelActionType.GrcLowerLetter:
           value = this.getNextGrcLetter(value!, true);
           break;
-        case LabelActionValueType.GrcUpperLetter:
+        case LabelActionType.GrcUpperLetter:
           value = this.getNextGrcLetter(value!, false);
           break;
         default:
