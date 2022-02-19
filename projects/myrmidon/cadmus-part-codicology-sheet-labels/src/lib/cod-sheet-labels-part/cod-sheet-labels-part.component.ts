49eb3254628043ebb82f5cc9442b988e9d7726bc
Myrmex <dfusi@hotmail.com>
2022-02-19 18:58:54 +0100

    Thesauri in labels part editor

--- projects/myrmidon/cadmus-part-codicology-sheet-labels/src/lib/cod-sheet-labels-part/cod-sheet-labels-part.component.ts
+++ projects/myrmidon/cadmus-part-codicology-sheet-labels/src/lib/cod-sheet-labels-part/cod-sheet-labels-part.component.ts
@@ -13,7 +13,11 @@ import {
 
 /**
  * CodSheetLabels part editor component.
- * Thesauri: TODO thesauri names and optionality
+ * Thesauri: cod-catchwords-positions, cod-numbering-systems,
+ * cod-numbering-techniques, cod-numbering-positions,
+ * cod-numbering-colors, cod-quiresig-systems,
+ * cod-quiresig-positions, cod-endleaf-materials, chronotope-tags,
+ * assertion-tags, doc-reference-types, doc-reference-tags.
  */
 @Component({
   selector: 'cadmus-cod-sheet-labels-part',
@@ -26,13 +30,40 @@ export class CodSheetLabelsPartComponent
 {
   // TODO form controls (form: FormGroup is inherited)
 
-  // TODO thesauri entries, e.g.:
-  // public tagEntries: ThesaurusEntry[] | undefined;
+  // C-COL
+  // cod-catchwords-positions
+  public poscEntries: ThesaurusEntry[] | undefined;
+  // N-COL
+  // cod-numbering-systems
+  public sysnEntries: ThesaurusEntry[] | undefined;
+  // cod-numbering-techniques
+  public techEntries: ThesaurusEntry[] | undefined;
+  // cod-numbering-positions
+  public posnEntries: ThesaurusEntry[] | undefined;
+  // cod-numbering-colors
+  public clrEntries: ThesaurusEntry[] | undefined;
+  // R/S-COL
+  // cod-quiresig-systems
+  public syssEntries: ThesaurusEntry[] | undefined;
+  // cod-quiresig-positions
+  public possEntries: ThesaurusEntry[] | undefined;
+  // ENDLEAF
+  // cod-endleaf-materials
+  public matEntries: ThesaurusEntry[] | undefined;
+  // chronotope-tags
+  public ctTagEntries: ThesaurusEntry[] | undefined;
+  // assertion-tags
+  public assTagEntries: ThesaurusEntry[] | undefined;
+  // doc-reference-types
+  public refTypeEntries: ThesaurusEntry[] | undefined;
+  // doc-reference-tags
+  public refTagEntries: ThesaurusEntry[] | undefined;
 
   constructor(authService: AuthJwtService, formBuilder: FormBuilder) {
     super(authService);
     // form
-    // TODO build controls and set this.form
+    // TODO
+    this.form = formBuilder.group({});
   }
 
   public ngOnInit(): void {
@@ -53,14 +84,78 @@ export class CodSheetLabelsPartComponent
   }
 
   protected override onThesauriSet(): void {
-    // TODO set entries from this.thesauri, e.g.:
-    // const key = 'note-tags';
-    // if (this.thesauri && this.thesauri[key]) {
-    // this.tagEntries = this.thesauri[key].entries;
-    // } else {
-    //   this.tagEntries = undefined;
-    // }
-    // if not using any thesauri, just remove this function
+    let key = 'cod-catchwords-positions';
+    if (this.thesauri && this.thesauri[key]) {
+      this.poscEntries = this.thesauri[key].entries;
+    } else {
+      this.poscEntries = undefined;
+    }
+    key = 'cod-numbering-systems';
+    if (this.thesauri && this.thesauri[key]) {
+      this.sysnEntries = this.thesauri[key].entries;
+    } else {
+      this.sysnEntries = undefined;
+    }
+    key = 'cod-numbering-techniques';
+    if (this.thesauri && this.thesauri[key]) {
+      this.techEntries = this.thesauri[key].entries;
+    } else {
+      this.techEntries = undefined;
+    }
+    key = 'cod-numbering-positions';
+    if (this.thesauri && this.thesauri[key]) {
+      this.posnEntries = this.thesauri[key].entries;
+    } else {
+      this.posnEntries = undefined;
+    }
+    key = 'cod-numbering-colors';
+    if (this.thesauri && this.thesauri[key]) {
+      this.clrEntries = this.thesauri[key].entries;
+    } else {
+      this.clrEntries = undefined;
+    }
+    key = 'cod-quiresig-systems';
+    if (this.thesauri && this.thesauri[key]) {
+      this.syssEntries = this.thesauri[key].entries;
+    } else {
+      this.syssEntries = undefined;
+    }
+    key = 'cod-quiresig-positions';
+    if (this.thesauri && this.thesauri[key]) {
+      this.possEntries = this.thesauri[key].entries;
+    } else {
+      this.possEntries = undefined;
+    }
+    key = 'cod-endleaf-materials';
+    if (this.thesauri && this.thesauri[key]) {
+      this.matEntries = this.thesauri[key].entries;
+    } else {
+      this.matEntries = undefined;
+    }
+    key = 'chronotope-tags';
+    if (this.thesauri && this.thesauri[key]) {
+      this.ctTagEntries = this.thesauri[key].entries;
+    } else {
+      this.ctTagEntries = undefined;
+    }
+    key = 'assertion-tags';
+    if (this.thesauri && this.thesauri[key]) {
+      this.assTagEntries = this.thesauri[key].entries;
+    } else {
+      this.assTagEntries = undefined;
+    }
+    key = 'doc-reference-types';
+    if (this.thesauri && this.thesauri[key]) {
+      this.refTypeEntries = this.thesauri[key].entries;
+    } else {
+      this.refTypeEntries = undefined;
+    }
+    key = 'doc-reference-tags';
+    if (this.thesauri && this.thesauri[key]) {
+      this.refTagEntries = this.thesauri[key].entries;
+    } else {
+      this.refTagEntries = undefined;
+    }
   }
 
   protected getModelFromForm(): CodSheetLabelsPart {
