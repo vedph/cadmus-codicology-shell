ee18f58320157e704f99ab8db4e140dae308267f
Myrmex <dfusi@hotmail.com>
2022-03-09 22:17:35 +0100

    Upgraded Angular

--- publish.bat
+++ publish.bat
@@ -10,6 +10,59 @@ call npm publish --access=public
 cd ..\..\..
 pause
 
-REM add other libraries...
+cd .\dist\myrmidon\cadmus-part-codicology-bindings
+call npm publish --access=public
+cd ..\..\..
+pause
+
+cd .\dist\myrmidon\cadmus-part-codicology-contents
+call npm publish --access=public
+cd ..\..\..
+pause
+
+cd .\dist\myrmidon\cadmus-part-codicology-decorations
+call npm publish --access=public
+cd ..\..\..
+pause
+
+cd .\dist\myrmidon\cadmus-part-codicology-edits
+call npm publish --access=public
+cd ..\..\..
+pause
+
+cd .\dist\myrmidon\cadmus-part-codicology-hands
+call npm publish --access=public
+cd ..\..\..
+pause
+
+cd .\dist\myrmidon\cadmus-part-codicology-layouts
+call npm publish --access=public
+cd ..\..\..
+pause
+
+cd .\dist\myrmidon\cadmus-part-codicology-material-dsc
+call npm publish --access=public
+cd ..\..\..
+pause
+
+cd .\dist\myrmidon\cadmus-part-codicology-sheet-labels
+call npm publish --access=public
+cd ..\..\..
+pause
+
+cd .\dist\myrmidon\cadmus-part-codicology-shelfmarks
+call npm publish --access=public
+cd ..\..\..
+pause
+
+cd .\dist\myrmidon\cadmus-part-codicology-watermarks
+call npm publish --access=public
+cd ..\..\..
+pause
+
+cd .\dist\myrmidon\cadmus-part-codicology-pg
+call npm publish --access=public
+cd ..\..\..
+pause
 
 echo ALL DONE
