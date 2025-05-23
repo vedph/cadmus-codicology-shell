ad97dd945f97fc9d78c7885c03d7182986ff53f5
Myrmex <dfusi@hotmail.com>
2025-05-23 20:22:12 +0200

    Update index-lookup-definitions.ts

--- src/app/index-lookup-definitions.ts
+++ src/app/index-lookup-definitions.ts
@@ -1,9 +1,15 @@
 import { IndexLookupDefinitions } from '@myrmidon/cadmus-core';
 import { METADATA_PART_TYPEID } from '@myrmidon/cadmus-part-general-ui';
 
+import { COD_DECORATIONS_PART_TYPEID } from '../../projects/myrmidon/cadmus-part-codicology-decorations/src/public-api';
+
 export const INDEX_LOOKUP_DEFINITIONS: IndexLookupDefinitions = {
   metadata_eid: {
     typeId: METADATA_PART_TYPEID,
     name: 'eid',
+  },
+  decor_elem_key: {
+    typeId: COD_DECORATIONS_PART_TYPEID,
+    name: 'element-key',
   },
 };
