29bcc73064796b521ee98ff263e73ab21e2208b5
Myrmex <dfusi@hotmail.com>
2026-03-01 10:26:18 +0100

    Formula demo

--- src/app/formula-demo-page/formula-demo-page.ts
+++ src/app/formula-demo-page/formula-demo-page.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { JsonPipe } from '@angular/common';
 
 import { MatCardModule } from '@angular/material/card';
@@ -15,14 +15,14 @@ import {
   styleUrl: './formula-demo-page.scss',
 })
 export class FormulaDemoPageComponent {
-  public data: CodLayoutFormulaWithDimensions = {
+  public readonly data = signal<CodLayoutFormulaWithDimensions>({
     prefix: 'IT',
     formula:
       '250 × 160 = 30 / 5 [170 / 5] 40 × 15 [5 / 50 / 5* (20) 5 / 40] 5 / 15',
     dimensions: [{ tag: 'custom', value: 10, unit: 'mm' }],
-  };
+  });
 
   public onDataChange(data: CodLayoutFormulaWithDimensions): void {
-    this.data = data;
+    this.data.set(data);
   }
 }
