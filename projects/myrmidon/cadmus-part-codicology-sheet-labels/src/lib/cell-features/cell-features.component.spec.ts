282cf6472c04b3b459637163381d7ce39efab185
Myrmex <dfusi@hotmail.com>
2026-03-12 10:39:27 +0100

    Step help

--- projects/myrmidon/cadmus-part-codicology-sheet-labels/src/lib/cell-features/cell-features.component.spec.ts
+++ projects/myrmidon/cadmus-part-codicology-sheet-labels/src/lib/cell-features/cell-features.component.spec.ts
@@ -1,9 +1,15 @@
 import { render } from '@testing-library/angular';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { CellFeaturesComponent } from './cell-features.component';
 
 describe('CellFeaturesComponent', () => {
   it('should create', async () => {
-    const { fixture } = await render(CellFeaturesComponent);
+    const { fixture } = await render(CellFeaturesComponent, {
+      providers: [
+        { provide: MatDialogRef, useValue: { close: () => {} } },
+        { provide: MAT_DIALOG_DATA, useValue: { flags: [], checkedIds: [] } },
+      ],
+    });
     expect(fixture.componentInstance).toBeTruthy();
   });
 });
